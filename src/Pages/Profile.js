2935d5f0087933cb4c9811a83f1274da96f249f6
Abu Taher <writerabutaher@gmail.com>
2022-11-10 04:47:22 +0600

    log in added

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -16,7 +16,7 @@ const Profile = () => {
                     user?.photoURL ?
                         <img src={user.photoURL} alt="" className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square" />
                         :
-                        <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" alt="" className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square" />
+                        <img src="https://cdn-icons-png.flaticon.com/512/560/560277.png" alt="" className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square" />
                 }
                 <div className="space-y-4 text-center divide-y divide-gray-700">
                     {
@@ -30,12 +30,47 @@ const Profile = () => {
                 <div className='mx-auto my-2'>
                     {
                         user ?
-                            <button onClick={handleLogOut} className='btn'>Log Out</button>
+                            <button onClick={handleLogOut}
+                                className="inline-block rounded border border-current px-8 py-3 text-sm font-medium text-indigo-600 transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:text-indigo-500"
+                            >
+                                Log Out
+                            </button>
                             :
-                            <Link to={'/login'} className='btn'>Log In</Link>
+                            <Link to={'/login'}
+                                className="inline-block rounded bg-indigo-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-indigo-500"
+                            >
+                                Log In
+                            </Link>
                     }
                 </div>
-                <Link to={'/home'} className='btn btn-primary'>Go To Home</Link>
+                <Link to={'/home'}
+                    className="group flex items-center justify-between rounded-lg border border-indigo-600 bg-indigo-600 px-5 py-3 transition-colors hover:bg-transparent focus:outline-none focus:ring"
+                >
+                    <span
+                        className="font-medium text-white transition-colors group-hover:text-indigo-600 group-active:text-indigo-500"
+                    >
+                        Go To Home
+                    </span>
+
+                    <span
+                        className="ml-4 flex-shrink-0 rounded-full border border-current bg-white p-2 text-indigo-600 group-active:text-indigo-500"
+                    >
+                        <svg
+                            className="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M17 8l4 4m0 0l-4 4m4-4H3"
+                            />
+                        </svg>
+                    </span>
+                </Link>
             </div>
         </div>
     );
